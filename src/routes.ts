71daf1f781333e271b12522cdb5c6cde7bf3ae20
Mário Cesar Cunha <mc.fialho@hotmail.com>
2022-08-08 16:33:11 -0300

    first

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import CreateCourseService from './CreateCourseService';
+
+export function createCourse(req: Request, res: Response) {
+  CreateCourseService.execute({
+    name: "Node", 
+    educator: "Cesar", 
+    duration: 10
+  });
+  CreateCourseService.execute({
+    name: "React", 
+    educator: "Dione", 
+  });
+
+  return res.send();
+}
+
