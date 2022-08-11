8c8d77f53b95814cdab9521c1ca353ba9339b681
Mário Cesar Cunha <mc.fialho@hotmail.com>
2022-08-11 08:22:02 -0300

    atualização com leitura de file e upload de arquivo

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express"; 
 //importa o Router do express que permite usar as rotas neste aquivo e depois exportar a variavel que recebe o Router(), assim todas as rotas são criadas aqui e invocadas no server.ts
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importUseCase";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp"
+})
 
 categoriesRoutes.post('/', (req, res) => {
     return createCategoryController.handle(req, res);
@@ -12,6 +18,10 @@ categoriesRoutes.post('/', (req, res) => {
 
 categoriesRoutes.get('/', (req, res) => {
     return listCategoriesController.handle(req, res);
+})
+
+categoriesRoutes.post("/import", upload.single("file"),(req, res) => {
+  return importCategoryController.handle(req, res);
 })
 
 export { categoriesRoutes };
