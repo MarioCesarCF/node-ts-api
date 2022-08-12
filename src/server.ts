6e6a461753f470f587339833ca6dfa0449f0f98f
Mário Cesar Cunha <mc.fialho@hotmail.com>
2022-08-12 10:55:47 -0300

    documentação com swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from "./swagger.json";
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json()); //necessário para ler as informações do body em formato json ou algo assim
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router);
 
